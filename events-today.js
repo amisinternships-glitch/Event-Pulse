02a9cc95d7066ae2345edcf3c3f961cbf39dbf10
amisinternships-glitch <amisinternships@gmail.com>
2026-04-06 11:55:15 -0500

    Add files via upload

--- events-today.js
+++ events-today.js
@@ -1,1 +1,25 @@
+const { getEventsPayload } = require("../../lib/events-feed");
 
+exports.handler = async function handler() {
+  try {
+    const payload = await getEventsPayload();
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(payload),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        error: "Unable to load events feed.",
+        detail: error.message,
+      }),
+    };
+  }
+};
